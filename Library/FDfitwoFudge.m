5f3908c1963685edc07425e4f850af860c5bfd88
ZhenjieYan <zhenjie@mit.edu>
2016-08-31 21:59:25 -0400

    quick bug fix

--- Library/FDfitwoFudge.m
+++ Library/FDfitwoFudge.m
@@ -1,6 +1,6 @@
 function [P,ffit,ErrorModelInfo]=FDfitwoFudge(k,f)
-    FDfun=@(P,k) 1./(exp(P(2)*(k.^2/P(3)-1))+1);
-    P0=[1,2,1];
+    FDfun=@(P,k) 1./(exp(P(1)*(k.^2/P(2)-1))+1);
+    P0=[2,1];
     [P,R,J,CovB,MSE,ErrorModelInfo]=nlinfit(k,f,FDfun,P0);
     [ffit]=FDfun(P,k);
 end
